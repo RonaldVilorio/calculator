d4433bd172fafd0de33bf1c07b816e86249aeeca
RonaldVilorio <ronvilorio@gmail.com>
2020-06-06 19:35:17 -0400

    toggle button to display on/off done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
-function startButton() {
-  alert("hey");
+function toggleButton() {
+  let toggle = document.querySelector("#displayNumber").textContent;
+//   if turned On and has content then turn off
+  if (toggle.length > 0) {
+    document.querySelector("#displayNumber").textContent = "";
+    // if turned off and no content then turn on with "0"
+  } else if (toggle.length === 0) {
+    document.querySelector("#displayNumber").textContent = "0";
+  }
 }
+// function pressNum(){
+//     document.querySelector()
+//     document.querySelectorAll(".display").textContent =
+
+// }
