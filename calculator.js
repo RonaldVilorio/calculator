3a5f5d9b0ee27909474d4ae8b0eb5223896ac0a1
RonaldVilorio <ronvilorio@gmail.com>
2020-06-06 23:23:44 -0400

    changed first row into buttons,refactored toggle

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
-function toggleButton() {
+function handleToggle() {
   let toggle = document.querySelector("#displayNumber").textContent;
-//   if turned On and has content then turn off
+  //   if turned On and has content then turn off
   if (toggle.length > 0) {
     document.querySelector("#displayNumber").textContent = "";
     // if turned off and no content then turn on with "0"
@@ -8,8 +8,13 @@ function toggleButton() {
     document.querySelector("#displayNumber").textContent = "0";
   }
 }
-// function pressNum(){
-//     document.querySelector()
-//     document.querySelectorAll(".display").textContent =
+// function handleNumClick(e) {
+//   let number = e.target.textContent;
+//   document.querySelector("#displayNumber").textContent = number;
+// }
+document.querySelector("#onButton").addEventListener("click", handleToggle);
 
+// let buttons = document.querySelectorAll("button");
+// for (button in buttons) {
+//   button.addEventListener("click",handleNumClick)
 // }
