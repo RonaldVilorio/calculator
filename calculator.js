807059217ee40386470335b518c3717cf39d444b
RonaldVilorio <ronvilorio@gmail.com>
2020-06-09 20:18:38 -0400

    fixed double dots next to each other after pressing = button

--- calculator.js
+++ calculator.js
@@ -12,58 +12,64 @@ let dotTracker = 0;
 // 0 has not clicked calculate
 // 1 has clicked calculate
 let displayBox = document.querySelector("#displayBox");
+let dotButton = document.querySelector("#dotButton");
 function clear() {
+  dotButton.classList.remove("disableButton");
   displayBox.textContent = "";
 }
 function handleDotClick(e) {
+  // still contains multiple dots in screen if toggle between =
+  // if dot exitsts already don't add more dots
+  dotButton.classList.toggle("disableButton");
   let dot = e.target.textContent;
-  displayBox.textContent = displayBox.textContent + dot;
+  if (displayBox.textContent.indexOf(".") - 1 === ".") {
+    null;
+  } else {
+    displayBox.textContent = displayBox.textContent + dot;
+  }
   // add disable class to my dotButton
-  document.querySelector("#dotButton").classList.toggle("disableButton");
 }
 
 function handleNumClick(e) {
   // check to see if display has numbers already
   let userNum = e.target.textContent;
-  let displayNumber = document.querySelector("#displayBox");
+  // let displayN = document.querySelector("#displayBox");
   if (calcTracker === 1) {
     clear();
     calcTracker--;
   }
   // if calculator on do something
-
-  if (displayNumber.textContent === "0") {
-    displayNumber.textContent = userNum;
-  } else if (displayNumber.textContent.length >= 1) {
-    displayNumber.textContent = displayNumber.textContent + userNum;
+  if (displayBox.textContent === "0") {
+    displayBox.textContent = userNum;
+  } else if (displayBox.textContent.length >= 1) {
+    displayBox.textContent = displayBox.textContent + userNum;
   } else {
-    displayNumber.textContent = userNum;
+    displayBox.textContent = userNum;
   }
   // if off then buttons won't work, "null"
 }
 function handleOperationClick(e) {
-  document.querySelector("#dotButton").classList.toggle("disableButton");
+  dotButton.classList.toggle("disableButton");
   let operation = e.target.textContent;
-  displayOperation = document.querySelector("#displayBox");
+
   // ++ is a no, there has to be content first, no 2 ++ inside string
   if (
-    displayOperation.textContent.length >= 1 &&
-    displayOperation.textContent.indexOf(operation) === -1 &&
-    displayOperation.textContent.indexOf("×") === -1 &&
-    displayOperation.textContent.indexOf("+") === -1 &&
-    displayOperation.textContent.indexOf("−") === -1 &&
-    displayOperation.textContent.indexOf("÷") === -1
+    displayBox.textContent.length >= 1 &&
+    displayBox.textContent.indexOf(operation) === -1 &&
+    displayBox.textContent.indexOf("×") === -1 &&
+    displayBox.textContent.indexOf("+") === -1 &&
+    displayBox.textContent.indexOf("−") === -1 &&
+    displayBox.textContent.indexOf("÷") === -1
   ) {
-    displayOperation.textContent =
-      displayOperation.textContent + " " + operation + " ";
+    displayBox.textContent = displayBox.textContent + " " + operation + " ";
   } else {
     null;
   }
 }
 function handleCalc() {
-  document.querySelector("#dotButton").classList.toggle("disableButton");
-  let string = document.querySelector("#displayBox").textContent;
-  let arr = string.split(" ");
+  dotButton.classList.toggle("disableButton");
+
+  let arr = displayBox.textContent.split(" ");
   let operation = arr[1];
   let num1 = arr[0] * 1;
   let num2 = arr[2] * 1;
@@ -78,15 +84,13 @@ function handleCalc() {
   } else {
     newString = newString + num1 / num2;
   }
-  let displayBox = document.querySelector("#displayBox");
+  // let displayBox = document.querySelector("#displayBox");
   if (arr.length === 3) {
     displayBox.textContent = newString;
   } else {
     null;
   }
-
   calcTracker++;
-  dotTracker = 0;
 }
 document.querySelector("#resetButton").addEventListener("click", clear);
 document.querySelector("#calcButton").addEventListener("click", handleCalc);
@@ -100,6 +104,3 @@ let operations = document.querySelectorAll(".operation");
 for (operation of operations) {
   operation.addEventListener("click", handleOperationClick);
 }
-
-// the + will be the break between num1 and num2
-// capture the whole string and send it to function =
