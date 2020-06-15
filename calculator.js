8ac9c63513f26e60f4df83ab9ab5264174bd081c
RonaldVilorio <ronvilorio@gmail.com>
2020-06-14 23:06:16 -0400

    added new background image

--- calculator.js
+++ calculator.js
@@ -116,7 +116,6 @@ function handleCalc() {
   let num1 = arr[0] * 1;
   let num2 = arr[2] * 1;
   let solution = 0;
-  console.log(arr)
   //   copy and paste the symbol from the console into the if statements
   if (operation === "×") {
     solution = solution + num1 * num2;
@@ -124,7 +123,7 @@ function handleCalc() {
     solution = solution + (num1 + num2);
   } else if (operation === "−") {
     solution = solution + (num1 - num2);
-    console.log(solution)
+    console.log(solution);
   } else {
     solution = solution + num1 / num2;
   }
